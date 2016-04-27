2e52ea094cc09d29fef738f30eb3fa1bbd30a5ce
tcardy <thomasb.cardy@gmail.com>
2016-04-26 20:18:59 -0400

    Update grid.h

--- alt/grid.h
+++ alt/grid.h
@@ -1,11 +1,16 @@
+#ifndef GRID_H
+#define GRID_H
+
 include "pixel.h"
 
 class Grid
 {
   public:
     Pixel grid[][];
-  
+    
+    // Constructor
+    Grid(int x, int y);
 };
 
-Grid::Grid(int x, int y);
+#endif
 
