98b87d613911c3a2af9b968b855b53ee8da15b48
Talha Ehtasham <McMerrison@users.noreply.github.com>
2016-04-28 20:30:27 -0400

    Exclude glut and just use normal C++ to simulate a z-buffer

--- PixelPainters.cpp
+++ PixelPainters.cpp
@@ -1,153 +1,84 @@
 /*
 *
-* Author of skeleton code: Ali Badereddin
-* https://mycodelog.com/author/alibad/ (Author)
-* https://mycodelog.com/2015/10/08/opengl-freeglut-in-visual-studio-2015/ (Code)
-*
 * Modified by: Talha Ehtasham
-* New/altered methods:
-* display()
-* updateBuffer()
-* Basic example of Painter's Algorithm using glDrawPixels()
+* Basic simulation of Painter's Algorithm
 *
+* Creates a "screen" or array of pixels defined by a number and depth
+* initializes array with number value "0"
+* Updates subsection of array to lower depth (closer) with number "1"
+* Prints before and after images
 */
 
-#include <gl/freeglut.h>
-#include <stdlib>
+#include <stdlib.h>
+#include <stdio.h>
 #include <math.h>
 
-void init();
-void display(void);
-void centerOnScreen();
-void drawObject();
-
-//  define the window position on zbuffer
-int window_x;
-int window_y;
-
 //  variables representing the window size
-int window_width = 480;
-int window_height = 480;
+int width = 50;
+int height = 50;
 
 //Define a Pixel using rgb values
 struct Pixel {
-	float r;
-	float g;
-	float b;
+	int color;
 	float depth;
 };
 typedef Pixel Pixel;
 
-//  variable representing the window title
-char *window_title = "Painter's Algorithm";
+void updateBuffer(Pixel *zbuffer, int minWidth, int maxWidth, int minHeight, int maxHeight, int newc, float depth);
+void printBuffer(Pixel *zbuffer);
 
-//-------------------------------------------------------------------------
-//  Program Main method.
-//-------------------------------------------------------------------------
-void main(int argc, char **argv)
-{
-	// Initiliazers for the window including size and position
-	glutInit(&argc, argv);
-	centerOnScreen();
-	glutInitWindowSize(window_width, window_height);
-	glutInitWindowPosition(window_x, window_y);
-
-	// Make sure the window has a depth buffer
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGB);
-
-	// Tells program to make depth comparisons while drawing
-	// Updates depth buffer previously initialized
-	glEnable(GL_DEPTH_TEST);
-	glutCreateWindow(window_title);
-
-	// Set the callback functions
-	glutDisplayFunc(display);
-
-	//  Start GLUT event processing loop
-	glutMainLoop();
-}
-
-//-------------------------------------------------------------------------
-//  Set OpenGL program initial state.
-//-------------------------------------------------------------------------
-void init()
-{
-	//  Set the frame buffer clear color to black. 
-	glClearColor(0.0, 0.0, 0.0, 1.0);
-}
-
-//-------------------------------------------------------------------------
-//  This function is passed to glutDisplayFunc in order to display 
-//  OpenGL contents on the window.
-//-------------------------------------------------------------------------
-void display(void)
-{
-	//  Draw object
-	drawObject();
-
-	//  Swap contents of backward and forward frame buffers
-	glutSwapBuffers();
-}
-
-//-------------------------------------------------------------------------
-//  Draws our object.
-//-------------------------------------------------------------------------
-void drawObject()
+int main()
 {
 	//Allocate the array as one-dimensional using width and height
 	//Access can be made by multiplying row by column
 	//(i.e. zbuffer[r][c] == zbuffer[r*c])
 	Pixel *zbuffer = new Pixel[width*height];
 	
-	//Draw shapes from furthest to closest
+	//Initilaize depth values at 1 (furthest)
+	for (int i = 0; i < width; i ++) {
+		for (int j = 0; j < height; j++) {
+			zbuffer[i*j].color = 0;
+			zbuffer[i*j].depth = 1; 
+		}
+	}
 	
-	//Make the whole window black
-	//This is at depth 1, in the background
-	updateBuffer(zbuffer, 0, 0, 0, 0, width, 0, height, 1);
+	printBuffer(zbuffer);
 	
-	//Set up a square in the window
-	//This is at depth 0.7
-	updateBuffer(zbuffer, 1, 0, 0, 300, 400, 200, 400, 0.7);
+	updateBuffer(zbuffer, 20, 40, 20, 40, 1, 0);
 	
-	//Set up a rectangle in the window
-	//This is at depth 0.5
-	updateBuffer(zbuffer, 1, 0, 0, 100, 400, 350, 400, 0.5);
-	
-	//Set up a rectangle in the window over the square
-	//This is at depth 0.3
-	updateBuffer(zbuffer, 0, 0, 1, 200, 450, 400, 450, 0.3);
-	
-	
-	glDrawPixels(width,height,GL_RGB | GL_DEPTH_COMPONENT,GL_FLOAT,zbuffer);
-	
+	printBuffer(zbuffer);
+
 	//Free the memory after each refresh
 	delete(zbuffer);
 }
 
 /*
 * Uses a nested for loop to set the values for a select set of pixels
-* For each pixel, update depth values if new pixel is closer to view
+* Starting with the least depth (closest to zbuffer), draw pixels from front to back
+* Update pixel only if it has not been previously defined, then update depth
+* This is basically a Reverse Painter's Algorithm
 *
 */
-void updateBuffer(Pixel *zbuffer, float r, float g, float b, int minWidth, int maxWidth, int minHeight, int maxHeight, float depth)
+void updateBuffer(Pixel *zbuffer, int minWidth, int maxWidth, int minHeight, int maxHeight, int newc, float depth)
 {
 	for (int i = minWidth; i < maxWidth; i ++) {
 		for (int j = minHeight; j < maxHeight; j++) {
-			//For each new layer, certain pixels will be updated multiple times
-			zbuffer[i*j].r = r;
-			zbuffer[i*j].g = g;
-			zbuffer[i*j].b = b;
-			zbuffer[i*j].depth = depth;
+			//Draw if new depth is less than (closer) current depth
+			//Since we draw front to back, each pixel will only be written to a single time
+			if (depth < zbuffer[i*j].depth) {
+				zbuffer[i*j].color = newc;
+				zbuffer[i*j].depth = depth;
+			}
 		}
 	}
-	
 }
-//-------------------------------------------------------------------------
-//  This function sets the window x and y coordinates
-//  such that the window becomes centered
-//-------------------------------------------------------------------------
-void centerOnScreen()
-{
-	window_x = (glutGet(GLUT_SCREEN_WIDTH) - window_width) / 2;
-	window_y = (glutGet(GLUT_SCREEN_HEIGHT) - window_height) / 2;
+
+void printBuffer(Pixel *zbuffer) {
+	for (int i = 0; i < width; i ++) {
+		for (int j = 0; j < height; j++) {
+			printf("%d", zbuffer[i*j].color);
+		}
+		printf("\n");
+	}
 }
+
